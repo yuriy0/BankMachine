2c41c322fa908375987bef4be68c1d2abdfd0897
yuriy0 <toporoy@mcmaster.ca>
2015-11-10 13:44:44 -0500

    Add missing file...

---
+++ BankMachine/KeyPad.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace BankMachine
+{
+    partial class KeyPad
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.b_1 = new System.Windows.Forms.Button();
+            this.b_3 = new System.Windows.Forms.Button();
+            this.b_2 = new System.Windows.Forms.Button();
+            this.b_4 = new System.Windows.Forms.Button();
+            this.b_5 = new System.Windows.Forms.Button();
+            this.b_6 = new System.Windows.Forms.Button();
+            this.b_7 = new System.Windows.Forms.Button();
+            this.b_8 = new System.Windows.Forms.Button();
+            this.b_9 = new System.Windows.Forms.Button();
+            this.b_ok = new System.Windows.Forms.Button();
+            this.b_0 = new System.Windows.Forms.Button();
+            this.b_clr = new System.Windows.Forms.Button();
+            this.b_del = new System.Windows.Forms.Button();
+            this.b_dot = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // b_1
+            // 
+            this.b_1.Font = new System.Drawing.Font("DejaVu Sans", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b_1.Location = new System.Drawing.Point(3, 3);
+            this.b_1.Name = "b_1";
+            this.b_1.Size = new System.Drawing.Size(50, 51);
+            this.b_1.TabIndex = 0;
+            this.b_1.Text = "1";
+            this.b_1.UseVisualStyleBackColor = true;
+            this.b_1.Click += new System.EventHandler(this.numButtonClick);
+            // 
+            // b_3
+            // 
+            this.b_3.Font = new System.Drawing.Font("DejaVu Sans", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b_3.Location = new System.Drawing.Point(115, 3);
+            this.b_3.Name = "b_3";
+            this.b_3.Size = new System.Drawing.Size(50, 51);
+            this.b_3.TabIndex = 1;
+            this.b_3.Text = "3";
+            this.b_3.UseVisualStyleBackColor = true;
+            this.b_3.Click += new System.EventHandler(this.numButtonClick);
+            // 
+            // b_2
+            // 
+            this.b_2.Font = new System.Drawing.Font("DejaVu Sans", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b_2.Location = new System.Drawing.Point(59, 3);
+            this.b_2.Name = "b_2";
+            this.b_2.Size = new System.Drawing.Size(50, 51);
+            this.b_2.TabIndex = 2;
+            this.b_2.Text = "2";
+            this.b_2.UseVisualStyleBackColor = true;
+            this.b_2.Click += new System.EventHandler(this.numButtonClick);
+            // 
+            // b_4
+            // 
+            this.b_4.Font = new System.Drawing.Font("DejaVu Sans", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b_4.Location = new System.Drawing.Point(3, 60);
+            this.b_4.Name = "b_4";
+            this.b_4.Size = new System.Drawing.Size(50, 51);
+            this.b_4.TabIndex = 4;
+            this.b_4.Text = "4";
+            this.b_4.UseVisualStyleBackColor = true;
+            this.b_4.Click += new System.EventHandler(this.numButtonClick);
+            // 
+            // b_5
+            // 
+            this.b_5.Font = new System.Drawing.Font("DejaVu Sans", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b_5.Location = new System.Drawing.Point(59, 60);
+            this.b_5.Name = "b_5";
+            this.b_5.Size = new System.Drawing.Size(50, 51);
+            this.b_5.TabIndex = 4;
+            this.b_5.Text = "5";
+            this.b_5.UseVisualStyleBackColor = true;
+            this.b_5.Click += new System.EventHandler(this.numButtonClick);
+            // 
+            // b_6
+            // 
+            this.b_6.Font = new System.Drawing.Font("DejaVu Sans", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b_6.Location = new System.Drawing.Point(115, 60);
+            this.b_6.Name = "b_6";
+            this.b_6.Size = new System.Drawing.Size(50, 51);
+            this.b_6.TabIndex = 5;
+            this.b_6.Text = "6";
+            this.b_6.UseVisualStyleBackColor = true;
+            this.b_6.Click += new System.EventHandler(this.numButtonClick);
+            // 
+            // b_7
+            // 
+            this.b_7.Font = new System.Drawing.Font("DejaVu Sans", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b_7.Location = new System.Drawing.Point(3, 117);
+            this.b_7.Name = "b_7";
+            this.b_7.Size = new System.Drawing.Size(50, 51);
+            this.b_7.TabIndex = 6;
+            this.b_7.Text = "7";
+            this.b_7.UseVisualStyleBackColor = true;
+            this.b_7.Click += new System.EventHandler(this.numButtonClick);
+            // 
+            // b_8
+            // 
+            this.b_8.Font = new System.Drawing.Font("DejaVu Sans", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b_8.Location = new System.Drawing.Point(59, 117);
+            this.b_8.Name = "b_8";
+            this.b_8.Size = new System.Drawing.Size(50, 51);
+            this.b_8.TabIndex = 7;
+            this.b_8.Text = "8";
+            this.b_8.UseVisualStyleBackColor = true;
+            this.b_8.Click += new System.EventHandler(this.numButtonClick);
+            // 
+            // b_9
+            // 
+            this.b_9.Font = new System.Drawing.Font("DejaVu Sans", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b_9.Location = new System.Drawing.Point(115, 117);
+            this.b_9.Name = "b_9";
+            this.b_9.Size = new System.Drawing.Size(50, 51);
+            this.b_9.TabIndex = 8;
+            this.b_9.Text = "9";
+            this.b_9.UseVisualStyleBackColor = true;
+            this.b_9.Click += new System.EventHandler(this.numButtonClick);
+            // 
+            // b_ok
+            // 
+            this.b_ok.Font = new System.Drawing.Font("DejaVu Sans", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b_ok.Location = new System.Drawing.Point(86, 231);
+            this.b_ok.Name = "b_ok";
+            this.b_ok.Size = new System.Drawing.Size(80, 51);
+            this.b_ok.TabIndex = 9;
+            this.b_ok.Text = "OK";
+            this.b_ok.UseVisualStyleBackColor = true;
+            this.b_ok.Click += new System.EventHandler(this.b_ok_Click);
+            // 
+            // b_0
+            // 
+            this.b_0.Font = new System.Drawing.Font("DejaVu Sans", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b_0.Location = new System.Drawing.Point(59, 174);
+            this.b_0.Name = "b_0";
+            this.b_0.Size = new System.Drawing.Size(50, 51);
+            this.b_0.TabIndex = 10;
+            this.b_0.Text = "0";
+            this.b_0.UseVisualStyleBackColor = true;
+            this.b_0.Click += new System.EventHandler(this.numButtonClick);
+            // 
+            // b_clr
+            // 
+            this.b_clr.Font = new System.Drawing.Font("DejaVu Sans", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b_clr.Location = new System.Drawing.Point(3, 231);
+            this.b_clr.Name = "b_clr";
+            this.b_clr.Size = new System.Drawing.Size(80, 51);
+            this.b_clr.TabIndex = 11;
+            this.b_clr.Text = "X";
+            this.b_clr.UseVisualStyleBackColor = true;
+            this.b_clr.Click += new System.EventHandler(this.b_clr_Click);
+            // 
+            // b_del
+            // 
+            this.b_del.Font = new System.Drawing.Font("DejaVu Sans", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b_del.Location = new System.Drawing.Point(3, 174);
+            this.b_del.Name = "b_del";
+            this.b_del.Size = new System.Drawing.Size(50, 51);
+            this.b_del.TabIndex = 12;
+            this.b_del.Text = "DEL";
+            this.b_del.UseVisualStyleBackColor = true;
+            this.b_del.Click += new System.EventHandler(this.b_del_Click);
+            // 
+            // b_dot
+            // 
+            this.b_dot.Font = new System.Drawing.Font("DejaVu Sans", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b_dot.Location = new System.Drawing.Point(115, 174);
+            this.b_dot.Name = "b_dot";
+            this.b_dot.Size = new System.Drawing.Size(50, 51);
+            this.b_dot.TabIndex = 13;
+            this.b_dot.Text = ".";
+            this.b_dot.UseVisualStyleBackColor = true;
+            this.b_dot.Click += new System.EventHandler(this.b_dot_Click);
+            // 
+            // KeyPad
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.b_ok);
+            this.Controls.Add(this.b_dot);
+            this.Controls.Add(this.b_del);
+            this.Controls.Add(this.b_clr);
+            this.Controls.Add(this.b_0);
+            this.Controls.Add(this.b_9);
+            this.Controls.Add(this.b_8);
+            this.Controls.Add(this.b_7);
+            this.Controls.Add(this.b_6);
+            this.Controls.Add(this.b_5);
+            this.Controls.Add(this.b_4);
+            this.Controls.Add(this.b_2);
+            this.Controls.Add(this.b_3);
+            this.Controls.Add(this.b_1);
+            this.Name = "KeyPad";
+            this.Size = new System.Drawing.Size(169, 286);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button b_1;
+        private System.Windows.Forms.Button b_3;
+        private System.Windows.Forms.Button b_2;
+        private System.Windows.Forms.Button b_4;
+        private System.Windows.Forms.Button b_5;
+        private System.Windows.Forms.Button b_6;
+        private System.Windows.Forms.Button b_7;
+        private System.Windows.Forms.Button b_8;
+        private System.Windows.Forms.Button b_9;
+        private System.Windows.Forms.Button b_ok;
+        private System.Windows.Forms.Button b_0;
+        private System.Windows.Forms.Button b_clr;
+        private System.Windows.Forms.Button b_del;
+        private System.Windows.Forms.Button b_dot;
+    }
+}
